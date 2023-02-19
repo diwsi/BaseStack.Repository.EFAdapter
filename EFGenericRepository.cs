81043f972539112da6d314fc2697bddf568f89ad
diwsi <xenamorphx@gmail.com>
2023-02-19 18:28:37 +0300

    initial

---
+++ EFGenericRepository.cs
@@ -0,0 +1,80 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Repository;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace EFAdapter
+{
+    public class EFRePository<T> : IRepository<T> where T : class
+    {
+        private readonly IUOW uow;
+
+        private readonly DbSet<T> dbSet;
+
+        private DbContext context
+        {
+            get
+            {
+                return (DbContext)uow.Context;
+            }
+        }
+
+        public void Delete(object id)
+        {
+            T? entityToDelete = GetByID(id);
+            if (entityToDelete != null)
+            {
+                Delete(entityToDelete);
+            }
+        }
+
+        public IEnumerable<T> Get(Expression<Func<T, bool>>? filter = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, string includeProperties = "")
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        public T? GetByID(object id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Update(T entityToUpdate)
+        {
+            dbSet.Attach(entityToUpdate);
+            context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+
+        public EFRePository(IUOW uow)
+        {
+            this.uow = uow;
+            dbSet = context.Set<T>();
+        }
+
+    }
+}
---
+++ EFUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository;
+
+namespace EFAdapter
+{
+    public class EFUnitOfWork : IUOW
+    {
+        private readonly DbContext context;
+
+        public object Context => context;
+
+        public async Task Save()
+        {
+           await context.SaveChangesAsync();
+        }
+        public EFUnitOfWork(DbContext context)
+        {
+            this.context = context;
+        }
+    }
+}
